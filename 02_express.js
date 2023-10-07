e875a3466246249cc5b51bb647d64302484cf085
Pvaishna <vaishnavi.padre@researchwire.in>
2023-10-07 20:40:51 +0530

    commit all 2022

---
+++ 02_express.js
@@ -0,0 +1,4 @@
+//Express is a third party module in nodeJs. (Framework or REST framework)
+//Express framework used to develop the REST services. Ex., GET, POST, PUT, HEAD, DELETE, etc...
+//Downloaded express module using yarn or npm tool.
+//All modules are downloaded to node_modules folder in current path
