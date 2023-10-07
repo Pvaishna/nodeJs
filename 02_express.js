dcae764bec6198fc1435b3aa4484773c1e510c6f
Pvaishna <vaishnavi.padre@researchwire.in>
2023-10-07 21:07:10 +0530

    expressJs commit

--- 02_express.js
+++ 02_express.js
@@ -2,3 +2,31 @@
 //Express framework used to develop the REST services. Ex., GET, POST, PUT, HEAD, DELETE, etc...
 //Downloaded express module using yarn or npm tool.
 //All modules are downloaded to node_modules folder in current path
+
+//Import Express module
+let express = require("express");
+
+//create the REST object
+let app = express();
+//where app object is the REST object. i.e., app object is used to develop REST object
+
+//create GET request 
+app.get("/", (req, res) => {
+    res.status(200).json({"message": "default get request .....!"});
+});
+
+app.get("/demo", (req, res) => {
+    res.status(200).json({"message" : "data from mongodb data soon .....!"});
+});
+
+app.post("/", (req, res)=>{
+    res.status(200).json({"message": "default post request .....!"});
+});
+
+app.post("/demo", (req, res)=> {
+    res.status(200).json({"message" : "data from cassandradb data soon ...!"});
+});
+
+app.listen(8082, ()=> {
+    console.log("server is ready at port 8082");
+});
